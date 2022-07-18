3bae28ccd6acd788e48930f6af7e4d28d51ef7ef
Marina Sivilay <msivilay@gmail.com>
2022-07-18 01:42:32 -0700

    Fixed function multiplyArray() for problem 5

--- js/app.js
+++ js/app.js
@@ -118,12 +118,12 @@ IMPORTANT DETAIL: You may not use the arithmetic operator * in this function. To
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiplyArray() function and see if the test passes.*/
 
 // Write your code here
-function multiplyArray(multArr) { //eslint-disable-line
-
+function multiplyArray(multArr) {
+  return [sumAndMultiply(multArr[0], multArr[1], multArr[2])[1], 'The numbers ' + multArr + ' have a product of ' + sumAndMultiply(multArr[0], multArr[1], multArr[2])[1] + '.'];
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyArray(testArray);
+testMultiplyArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop.
 
